3edd080d6caf82a95ccefba7ac802ae1ca1384ce
yisus82 <djalma8@gmail.com>
2024-07-02 20:35:25 +0200

    Home screen

--- KMPMovies/composeApp/src/commonMain/kotlin/com/liceolapaz/jprf/kmpmovies/App.kt
+++ KMPMovies/composeApp/src/commonMain/kotlin/com/liceolapaz/jprf/kmpmovies/App.kt
@@ -1,14 +1,61 @@
 package com.liceolapaz.jprf.kmpmovies
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        Text("Hello, Compose!")
+        Surface(modifier = Modifier.fillMaxSize()) {
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(120.dp),
+                contentPadding = PaddingValues(4.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                movies.forEach { movie ->
+                    item {
+                        MovieItem(movie)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieItem(movie: Movie) {
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(2f / 3f)
+                .clip(MaterialTheme.shapes.small)
+                .background(MaterialTheme.colorScheme.primaryContainer),
+        )
+        Text(
+            text = movie.title,
+            style = MaterialTheme.typography.bodySmall,
+            maxLines = 1,
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
---
+++ KMPMovies/composeApp/src/commonMain/kotlin/com/liceolapaz/jprf/kmpmovies/Movie.kt
@@ -0,0 +1,15 @@
+package com.liceolapaz.jprf.kmpmovies
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val poster: String,
+)
+
+val movies = (1..100).map {
+    Movie(
+        id = it,
+        title = "Movie $it",
+        poster = "https://picsum.photos/200/300?random=$it",
+    )
+}
