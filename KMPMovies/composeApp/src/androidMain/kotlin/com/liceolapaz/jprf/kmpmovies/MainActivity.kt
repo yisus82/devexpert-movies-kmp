db631658ad7c0b8301a66a82d0274830a33710b2
yisus82 <djalma8@gmail.com>
2024-07-03 12:08:39 +0200

    Transparent status bar

--- KMPMovies/composeApp/src/androidMain/kotlin/com/liceolapaz/jprf/kmpmovies/MainActivity.kt
+++ KMPMovies/composeApp/src/androidMain/kotlin/com/liceolapaz/jprf/kmpmovies/MainActivity.kt
@@ -1,23 +1,33 @@
 package com.liceolapaz.jprf.kmpmovies
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
+            EnableTransparentStatusBar()
             App()
         }
     }
-}
 
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
+    @Composable
+    private fun EnableTransparentStatusBar() {
+        val view = LocalView.current
+        val isDarkMode = isSystemInDarkTheme()
+        if (!view.isInEditMode) {
+            val window = (view.context as MainActivity).window
+            window.statusBarColor = Color.TRANSPARENT
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !isDarkMode
+        }
+    }
 }
--- KMPMovies/composeApp/src/commonMain/kotlin/com/liceolapaz/jprf/kmpmovies/ui/screens/Screen.kt
+++ KMPMovies/composeApp/src/commonMain/kotlin/com/liceolapaz/jprf/kmpmovies/ui/screens/Screen.kt
@@ -1,14 +1,20 @@
 package com.liceolapaz.jprf.kmpmovies.ui.screens
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun Screen(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
-    MaterialTheme {
+    val isDarkMode = isSystemInDarkTheme()
+    val colorScheme = if (isDarkMode) darkColorScheme() else lightColorScheme()
+
+    MaterialTheme(colorScheme) {
         Surface(
             modifier = modifier.fillMaxSize(),
             content = content
